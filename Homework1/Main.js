b4c162e6e900d88d372a11ef877bb1787f62e7bc
artemkit22 <artem1357984@gmail.com>
2023-05-02 11:33:49 +0300

    add leson 1 to project JShW.html Main.js

---
+++ Homework1/Main.js
@@ -0,0 +1,57 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+
+
+//
+// let word = 'hello'
+// let company = 'owu'
+// let end = 'com'
+// let country = 'ua'
+// let one = 1
+// let ten = 10
+// let minus = -999
+// let oneTwoThree = 123
+// let pi = 3.14
+// let drob = 2.7
+// let sixteen = 16
+// let boolt = true
+// let boolf = false
+//
+// console.log(word, company, end, country, one, ten, minus, oneTwoThree, pi, drob, sixteen, boolt, boolf)
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// let firstname = 'Artem'
+// let middlename = 'Kompaniiets'
+// let lastname = 'Viktorovych'
+//
+// console.log(firstname + ' ' + middlename + ' ' + lastname)
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100
+// let b = '100'
+// let c = true
+//
+// console.log(typeof a, typeof b, typeof c)
+
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// let name = prompt('What is your name:');
+// let lastname = prompt('What is your lastname:');
+// let years = prompt('how old are you:');
+//
+// console.log(name, lastname, years)
