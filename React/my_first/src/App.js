250b96b28e52c80b4c843fcf6aaefaf465f4947e
Yamy <Yamy19007@gmail.com>
2023-07-04 19:48:35 +0200

    add hm (Fix last commit)

---
+++ React/my_first/src/App.js
@@ -0,0 +1,106 @@
+import logo from './logo.svg';
+import './App.css';
+import Foo from './appp.jsx';
+import foo from "./appp.jsx";
+// function App() {
+//   return (
+//     <>
+//
+//         <Foo
+//           image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//           id = "0"
+//           name = "Gomer"
+//           surname = "daun"
+//           spaice = "Yellow"
+//           gender = "male"
+//       />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             id = "1"
+//             name = "huj"
+//             surname = "jopa"
+//             spaice = "black"
+//             gender = "female"
+//         />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             id = "2"
+//             name = "vasya"
+//             surname = "debil"
+//             spaice = "bleu"
+//             gender = "male"
+//         />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             id = "3"
+//             name = "petya"
+//             surname = "jeblan"
+//             spaice = "orange"
+//             gender = "female"
+//         />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             id = "4"
+//             name = "danya"
+//             surname = "kon"
+//             spaice = "green"
+//             gender = "female"
+//         />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             id = "5"
+//             name = "jopa"
+//             surname = "chlen"
+//             spaice = "zelenuy"
+//             gender = "male"
+//         />
+//
+//
+//
+//     </>
+//   );
+// }
+// export default App;
+// function App() {
+//   return (
+//     <>
+//         <Foo
+//                         image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//                         name = "Вася"
+//                         surname = "Пупкин"
+//                         age = "25"
+//                         gender = "Чоловік"
+//                     />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             name = "Олег"
+//             surname = "Віннік"
+//             age = "1234"
+//             gender = "чоловік"
+//         />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             name = "Кокос"
+//             surname = "Шляпович"
+//             age = "234"
+//             gender = "хз"
+//         />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             name = "asdf"
+//             surname = "Андріївна"
+//             age = "25"
+//             gender = "жінка"
+//         />
+//         <Foo
+//             image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/11/16/12/homer-simpson.jpeg?width=1200"
+//             name = "Влад"
+//             surname = "Гантеля"
+//             age = "11"
+//             gender = "чоловік"
+//         />
+//
+//     </>
+//          );
+//         }
+//         export default App;
---
+++ React/my_first/src/appp.jsx
@@ -0,0 +1,29 @@
+// let foo = (props) =>{
+//     return (
+//         <div>
+//             <img src={props.image}/>
+//             <div>id: {props.id}</div>
+//             <div>id: {props.name}</div>
+//             <div>id: {props.surname}</div>
+//             <div>id: {props.spaice}</div>
+//             <div>id: {props.gender}</div>
+//         </div>
+//     );
+// }
+//
+// export default foo;
+
+// let foo = (props) =>{
+//     return (
+//         <div>
+//             <img src={props.image}/>
+//             <div>Імя: {props.name}</div>
+//             <div>Призвіще: {props.surname}</div>
+//             <div>Вік: {props.age}</div>
+//             <div>Стать: {props.gender}</div>
+//         </div>
+//     );
+// }
+//
+// export default foo;
+
